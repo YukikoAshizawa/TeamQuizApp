73c90a913554a631cca32d66e0b506a3c1261b9a
YukikoAshizawa <uni202607_01@telework.to>
2026-09-15 15:25:03 +0900

    フォーム

--- TeamQuizApp/Form1.cs
+++ TeamQuizApp/Form1.cs
@@ -1,10 +1,26 @@
+using System;
+using System.Windows.Forms;
+
 namespace TeamQuizApp
 {
     public partial class Form1 : Form
     {
+        private readonly QuestionLoader loader;
+        private readonly AnswerChecker checker;
+        private readonly ScoreManager score;
+        private readonly UiUpdater ui;
+
+        private Question current;
+
+
         public Form1()
         {
             InitializeComponent();
+
+            loader = new QuestionLoader();
+            checker = new AnswerChecker();
+            score = new ScoreManager();
+            ui = new UiUpdater();
         }
     }
 }
