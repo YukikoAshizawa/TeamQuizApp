3cded6a878a6835bf3488060dcc91ef764e4f8d1
YukikoAshizawa <uni202607_01@telework.to>
2026-09-15 15:17:15 +0900

    AnswerChecker.csの作成

---
+++ TeamQuizApp/AnswerChecker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamQuizApp
+{
+    internal class AnswerChecker
+    {
+        public bool CheckAnswer(Question q, int selectedIndex)
+        {
+            return q.CorrectIndex == selectedIndex;
+        }
+    }
+}
