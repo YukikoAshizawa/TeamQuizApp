163d40287dc171e5686ca01b5175ca44eda5ae1a
KensukeSaito2026 <uni202607_12@telework.to>
2026-09-15 15:52:16 +0900

    Create QuestionLoader.cs

---
+++ TeamQuizApp/QuestionLoader.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace TeamQuizApp
+{
+    public class  Question
+    {
+        public string Text { get; set; } = "";
+        public string[] Choices { get; set; } = new string[4];
+        public int CorrectIndex { get; set; }
+
+    }
+    public class QuestionLoader
+    {
+        private readonly List<Question> _questions = new List<Question>();
+        private readonly Random _rand = new Random();
+
+        public QuestionLoader(string path = "questions.csv")
+        {
+            string csvPath = Path.Combine(AppContext.BaseDirectory,path);
+            foreach (var line in File.ReadAllLines(csvPath))
+            {
+                if (line.StartsWith("question")) continue;
+                var cols = line.Split(',');
+                _questions.Add(new Question
+                {
+                    Text = cols[0],
+                    Choices = new[] { cols[1], cols[2], cols[3], cols[4] },
+                    CorrectIndex = int.Parse(cols[5])
+                });
+            }
+        }
+
+
+        public Question GetRandomQuestion()
+        {
+            int idx = _rand.Next(_questions.Count);
+            return _questions[idx];
+        }
+    }
+}
