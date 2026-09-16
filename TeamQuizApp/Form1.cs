3a1b8f2293e8bb7be579fc4b54f9a60fd7a5fdec
YukikoAshizawa <uni202607_01@telework.to>
2026-09-16 09:34:23 +0900

    Form1.csの実装

--- TeamQuizApp/Form1.cs
+++ TeamQuizApp/Form1.cs
@@ -12,7 +12,6 @@ namespace TeamQuizApp
 
         private Question current;
 
-
         public Form1()
         {
             InitializeComponent();
@@ -20,7 +19,31 @@ namespace TeamQuizApp
             loader = new QuestionLoader();
             checker = new AnswerChecker();
             score = new ScoreManager();
-            ui = new UiUpdater();
+            ui = new UiUpdater(questionLabel,
+                new[] { answerButton1, answerButton2, answerButton3, answerButton4 },
+                logListBox);
+
+            LoadNextQuestion();
+        }
+
+        private void answerButton_Click(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+            int index = Array.IndexOf(new[] { answerButton1, answerButton2, answerButton3, answerButton4 }, btn);
+
+            bool result = checker.CheckAnswer(current, index);
+            score.Record(result);
+
+            ui.LogResult(result ? "正解！" : "不正解...");
+            ui.LogResult(score.GetResult());
+
+            LoadNextQuestion();
+        }
+
+        private void LoadNextQuestion()
+        {
+            current = loader.GetRandomQuestion();
+            ui.ShowQuestion(current);
         }
     }
 }
