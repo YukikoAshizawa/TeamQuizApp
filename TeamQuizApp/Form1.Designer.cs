8db02b71e4d1b487a9183ccf43d437d42fd45c1a
YukikoAshizawa <uni202607_01@telework.to>
2026-09-16 10:39:31 +0900

    Buttonのイベントを追加

--- TeamQuizApp/Form1.Designer.cs
+++ TeamQuizApp/Form1.Designer.cs
@@ -53,6 +53,7 @@
             answerButton1.TabIndex = 1;
             answerButton1.Text = "button1";
             answerButton1.UseVisualStyleBackColor = true;
+            answerButton1.Click += answerButton_Click;
             // 
             // answerButton2
             // 
@@ -62,6 +63,7 @@
             answerButton2.TabIndex = 2;
             answerButton2.Text = "button2";
             answerButton2.UseVisualStyleBackColor = true;
+            answerButton2.Click += answerButton_Click;
             // 
             // answerButton3
             // 
@@ -71,6 +73,7 @@
             answerButton3.TabIndex = 3;
             answerButton3.Text = "button3";
             answerButton3.UseVisualStyleBackColor = true;
+            answerButton3.Click += answerButton_Click;
             // 
             // answerButton4
             // 
@@ -80,6 +83,7 @@
             answerButton4.TabIndex = 4;
             answerButton4.Text = "button4";
             answerButton4.UseVisualStyleBackColor = true;
+            answerButton4.Click += answerButton_Click;
             // 
             // logListBox
             // 
